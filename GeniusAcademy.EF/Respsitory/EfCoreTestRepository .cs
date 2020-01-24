cc06d433990e77ad51ac4934770f574bc44f5d89
Sneh Desai <45747712+SnehDesai1096@users.noreply.github.com>
2020-01-25 01:22:12 +0530

    Add files via upload

---
+++ GeniusAcademy.EF/Respsitory/EfCoreTestRepository .cs
@@ -0,0 +1,18 @@
+﻿using GeniusAcademy.Data;
+using GeniusAcademy.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeniusAcademy.EF.Respsitory
+{
+    public class EfCoreTestRepository : EfCoreRepository<Test, DataContext>
+    {
+        public EfCoreTestRepository(DataContext context) : base(context)
+        {
+
+        }
+
+       
+    }
+}
---
+++ GeniusAcademy.EF/Respsitory/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using GeniusAcademy.Types;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeniusAcademy.EF.Respsitory
+{
+    public interface IRepository<T> where T : class, IEntity
+    {   
+        Task<List<T>> GetAll();
+        Task<T> Get(long id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(long id);
+    }
+}
