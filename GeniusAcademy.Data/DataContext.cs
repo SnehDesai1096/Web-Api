23391b4ac4e0b9cb674563348dd892d3531bc4bc
Sneh Desai <45747712+SnehDesai1096@users.noreply.github.com>
2020-01-25 01:17:38 +0530

    Add files via upload

---
+++ GeniusAcademy.Data/DataContext.cs
@@ -0,0 +1,49 @@
+﻿using GeniusAcademy.Types;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeniusAcademy.Data
+{
+    
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+       
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Test> Test { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            //builder.Entity<IdentityUser>(entity => { entity.ToTable(name: "Users").Property(p => p.Id).HasColumnType("int"); });
+            builder.Entity<IdentityUser>(entity => { entity.ToTable(name: "Users"); });
+            builder.Entity<IdentityRole>(entity => { entity.ToTable(name: "Roles"); });
+            builder.Entity<IdentityUserRole<string>>(entity => { entity.ToTable("UserRoles"); });
+            builder.Entity<IdentityUserClaim<string>>(entity => { entity.ToTable("UserClaims"); });
+            builder.Entity<IdentityUserLogin<string>>(entity => { entity.ToTable("UserLogins"); });
+            builder.Entity<IdentityUserToken<string>>(entity => { entity.ToTable("UserTokens"); });
+            builder.Entity<IdentityRoleClaim<string>>(entity => { entity.ToTable("RoleClaims"); });
+
+
+            #region "Seed Data"
+            builder.Entity<IdentityRole>().HasData(
+                new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                  new { Id = "2", Name = "Agency", NormalizedName = "AGENCY" },
+                new { Id = "3", Name = "Employee", NormalizedName = "STUDENT" }
+                );
+
+            #endregion
+
+
+        }
+
+    }
+}
